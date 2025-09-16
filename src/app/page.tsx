0a5d99ea32a0ddb3995e7b25326d737e5580d1ad
Gustavo <maiagustavo125@gmail.com>
2025-09-16 18:45:51 -0300

    feat: implement Dashboard component with layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+
+
+export default function Dashboard() {
+    return (
+        <main className="bg-gray-100 min-h-screen">
+            <div className="px-28 py-14">
+                <h1 className="text-5xl font-bold text-neutral-800 mb-4">Dashboard</h1>
+                <p className="text-2xl">Última medição: 16/09/2025 - 6:00</p>
+            </div>
+        </main>
+    );
 }
