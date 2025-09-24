8f7391a47bd6b2f562ab19eccd53c585d7ed49b3
Gustavo <maiagustavo125@gmail.com>
2025-09-23 21:05:19 -0300

    no message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "Dashboard",
+  title: "MVP",
   description: "Projeto de Iniciação - SENAI CIMATEC",
 };
 
